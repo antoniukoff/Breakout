327c6f5cd498aea9189f842ce161683a5e9c81f3
antoniukoff <n01477554@humbermail.ca>
2025-03-14 02:12:24 -0400

    added input utilities

--- core/include/Input.h
+++ core/include/Input.h
@@ -1,10 +1,16 @@
 #pragma once
 #include <../vendor/glfw/include/GLFW/glfw3.h>
+#include <utility>
 
 class Input
 {
 public:
 	static bool is_key_pressed(int key);
 	static bool is_mouse_pressed(int button);
+
+	std::pair<float, float> get_mouse_pos();
+
+	std::pair<float, float> get_rel_mouse_pos();
+
 };
 
--- core/src/Input.cpp
+++ core/src/Input.cpp
@@ -14,3 +14,30 @@ bool Input::is_mouse_pressed(int button)
 	int state = glfwGetMouseButton(window, button);
 	return state == GLFW_PRESS || state == GLFW_RELEASE;
 }
+
+std::pair<float, float> Input::get_mouse_pos()
+{
+	auto window = Application::get().get_window()->get_handle();
+
+	double x, y;
+	glfwGetCursorPos(window, &x, &y);
+
+	return{ x, y };
+}
+
+std::pair<float, float> Input::get_rel_mouse_pos()
+{
+	auto window = Application::get().get_window()->get_handle();
+
+	int wx, wy;
+	glfwGetWindowSize(window, &wx, &wy);
+	int half_wx = wx / 2;
+	int half_wy = wy / 2;
+
+	double x, y;
+	glfwGetCursorPos(window, &x, &y);
+	float rel_x = (x - half_wx) / half_wx;
+	float rel_y = (y - half_wy) / half_wy;
+
+	return{ rel_x, rel_y };
+}
